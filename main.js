e76ba0a6930745fc3f8ec9cc839e33ab6de44426
Mary-fox <Mary-fox2008@yandex.ru>
2021-09-30 14:08:15 +0300

    создали main.js
    создали main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+//document.write ("<h1>Hello from JS</h1>");
+//let num = 0;
+//let age = prompt("Сколько вам лет?")
+//while (true) {
+//   if (age > 0 && age < 100) {
+//      break;
+ //  }
+  // age = prompt("Некорректный вод. Сколько вам лет?")
+// }
+//for (let i = 0; i < 10; i++) {
+// document.write(`<h4>${i}</h4>`);
+//}
+const person = {
+  firstName: "Mikhail",
+  lastName: "Nepomnyashchiy",
+  age:35,
+  city: "Moscow"
+};
