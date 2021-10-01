97cad95a3b780aa6d9c470ac6b7fa8059d0426aa
Mary-fox <Mary-fox2008@yandex.ru>
2021-10-01 14:06:34 +0300

    Добавили примеры с функциями

--- main.js
+++ main.js
@@ -10,14 +10,14 @@
 //for (let i = 0; i < 10; i++) {
 // document.write(`<h4>${i}</h4>`);
 //}
-const person = {
-  firstName: "Mikhail",
-  lastName: "Nepomnyashchiy",
-  age:35,
-  city: "Moscow"
-};
-person.age=36;
-console.log(person.age);
+// const person = {
+//   firstName: "Mikhail",
+//   lastName: "Nepomnyashchiy",
+//   age:35,
+//   city: "Moscow"
+// };
+// person.age=36;
+// console.log(person.age);
 
 //console.log(person.firstName);
 
@@ -25,8 +25,31 @@ console.log(person.age);
 //  console.log(key, person[key]);
 //}
 
-const nums = [1, 2, 3, 4];
-//console.log(nums[0]);
-// nums.push(5)
- console.log(nums);
- console.log(nums.length);
+// const nums = [1, 2, 3, 4];
+// //console.log(nums[0]);
+// // nums.push(5)
+//  console.log(nums);
+//  console.log(nums.length);
+
+// const sum = (a, b) => {
+//   console.log(a + b);
+// };
+
+// sum (1, 6);
+// sum (8, 22);
+
+// const sum = (a, b) => a + b;
+// const c = sum(8, 6);
+// console.log(c);
+// console.log(sum(4, 10));
+
+const age = prompt("Сколько Вам лет?");
+function isOldEnough(age) {
+  if (age >= 18) {
+    return true
+  }
+  return false;
+}
+
+const message = isOldEnough(age) ? "Welcome" : "Bye";
+alert(message);
